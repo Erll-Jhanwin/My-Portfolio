a9f18214d1d98175046a331277a860cbe6c13b49
Erll-Jhanwin <erlljhanwincatapang61@gmail.com>
2025-01-23 19:49:13 +0800

    first commit - navigation bar, responsive

---
+++ index.js
@@ -0,0 +1,26 @@
+// const toggleBtn = document.querySelector('.toggle_btn');
+// const toggleBtnIcon = document.querySelector('.toggle_btn i');
+// const dropDownMenu = document.querySelector('.dropdown_menu');
+
+// toggleBtn.onclick = function () {
+//     dropDownMenu.classList.toggle('open');
+//     const isOpen = dropDownMenu.classList.contains('open');;
+
+//     toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fas fa-bars';
+// }
+
+
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick = function () {
+    // Toggle the open class
+    dropDownMenu.classList.toggle('open');
+    
+    // Check if dropdown is open
+    const isOpen = dropDownMenu.classList.contains('open');
+    
+    // Change icon based on dropdown state
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+};
