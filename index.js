736d189e9d31589cc46b94db9e3832fdc8ce2215
Erll-Jhanwin <erlljhanwincatapang61@gmail.com>
2025-01-25 20:30:33 +0800

    second commit - adding homepage, picture

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// const toggleBtn = document.querySelector('.toggle_btn');
-// const toggleBtnIcon = document.querySelector('.toggle_btn i');
-// const dropDownMenu = document.querySelector('.dropdown_menu');
-
-// toggleBtn.onclick = function () {
-//     dropDownMenu.classList.toggle('open');
-//     const isOpen = dropDownMenu.classList.contains('open');;
-
-//     toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fas fa-bars';
-// }
-
-
 const toggleBtn = document.querySelector('.toggle_btn');
 const toggleBtnIcon = document.querySelector('.toggle_btn i');
 const dropDownMenu = document.querySelector('.dropdown_menu');
